6512d2a3b08ab8d9e3460f3fe5c2ba8bfb7a925a
jrwaheed <j.r.waheed@gmail.com>
2021-05-19 17:38:02 +0200

    Shapes updates

--- src/at/ran/games/firstgame/StarTrek.java
+++ src/at/ran/games/firstgame/StarTrek.java
@@ -1,7 +1,39 @@
 package at.ran.games.firstgame;
 
-public class StarTrek {
-    public static void main(String[] args) {
-        System.out.println("Spock");
+import org.newdawn.slick.*;
+
+public class StarTrek extends BasicGame {
+    private Rectangle rectangle;
+
+    public StarTrek(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+        this.rectangle = new Rectangle(100, 100, 5);
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+
+        this.rectangle.update(delta);
+
+    }
+
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        this.rectangle.render(graphics);
+    }
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new FirstShapes("Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
